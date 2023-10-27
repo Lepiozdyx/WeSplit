5f1fb8838759ffac320134464d0000798d690d4d
Alex Zhulai <Lepiozdyx@gmail.com>
2023-10-27 15:54:42 -0300

    Change the "Total Amount" text view to red using the conditional modifier.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -62,6 +62,7 @@ struct ContentView: View {
                 
                 Section("Total amount per check") {
                     Text(totalPerCheck, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(tipPercentage <= 0 ? .red : .black)
                 }
                 
                 Section("Amount per person") {
